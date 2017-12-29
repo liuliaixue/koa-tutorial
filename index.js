2abb8f04a374ae572fa883708ac91c5d800efe7f
alan.liu <sliu.modelo@outlook.com>
2017-12-29 15:20:12 +0800

    u

---
+++ index.js
@@ -0,0 +1,35 @@
+const Koa = require('koa');
+const app = new Koa();
+const router =  require('koa-router')();
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
+
+router.get('/hello/:name', async (ctx, next) => {
+  var name = ctx.params.name;
+  ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+  ctx.response.body = '<h1>Index</h1>';
+});
+
+app.use(router.routes());
+
+app.listen(3000);
